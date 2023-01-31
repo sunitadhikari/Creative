6224741e0b0c700d6c5e93eddda65114ae5b95b3
sunitadhikari <sunitaadhikari2001@gmail.com>
2023-01-31 11:17:47 +0545

    j

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import "./App.css";
-import Nav from "./Nav"
+import Nav from "./components/Nav"
 
 function App() {
   return (
---
+++ src/components/Nav.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <>
+      <header>
+        <div className="container flexx">
+          <div className="logo">
+            <h1><span>mm</span> uu  yu</h1>
+          </div>
+          <nav>
+            <button></button>
+           <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/">About</a></li>
+            <li><a href="/">Creative</a></li>
+            <li><a href="/">Education</a></li>
+            <li><a href="/">Works</a></li>
+            <li><a href="/">Blog</a></li>
+            <li><a href="/">Contact</a></li>
+
+
+           </ul>
+          </nav>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default Nav
