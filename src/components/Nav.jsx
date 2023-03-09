72b24b6cff77f669d063573c072c32269c8bd89a
sunitadhikari <sunitaadhikari2001@gmail.com>
2023-03-10 00:36:09 +0545

    jgkJ

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,7 +7,7 @@ const Nav = () => {
       <header>
         <div className="container flexx">
           <div className="logo">
-            <h1><span>fgfGJ</span>fdsff</h1>
+            <h1><span>fGJ</span>fdsff</h1>
           </div>
           <nav>
             <button className='toggle' onClick={() => setSidebar(!sidebar)}>
